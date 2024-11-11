6e584e1bf6b5be936309e62c7cd48cfdcf925a12
dibya.borah <dibyajyotiborah469@gmail.com>
2024-11-11 23:45:33 +0530

    once use case remaining of memoize fn

---
+++ memoize.js
@@ -0,0 +1,41 @@
+function memoize(fn) {
+    const argsCheckList = []
+    return function (...args) {
+        argsCheckList.push(...args)
+        if (argsCheckList.length > 4) {
+            argsCheckList.shift();
+            argsCheckList.shift();
+        }
+        if ((argsCheckList[0] === argsCheckList[2])
+            && (argsCheckList[1] === argsCheckList[3])) {
+            return argsCheckList[0] + argsCheckList[1];
+        } else {
+            return fn(...args)
+        }
+    };
+};
+
+let callCount = 0;
+const memoizeFn = memoize((a, b) => {
+    callCount += 1;
+    return a + b;
+});
+
+console.log(memoizeFn(2, 2));
+console.log(memoizeFn(2, 2));
+console.log(callCount);
+console.log(memoizeFn(1, 2));
+console.log(callCount);
+
+// [4,4, 1, 3, 2]
+
+/** 
+ * let callCount = 0;
+ * const memoizedFn = memoize(function (a, b) {
+ *	 callCount += 1;
+ *   return a + b;
+ * })
+ * memoizedFn(2, 3) // 5
+ * memoizedFn(2, 3) // 5
+ * console.log(callCount) // 1 
+ */
