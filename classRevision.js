79da2069961d9c95c6c782b127f9b738114e5e44
dibya.borah <dibyajyotiborah469@gmail.com>
2025-03-06 15:55:52 +0530

    Class + getters and setters

---
+++ classRevision.js
@@ -0,0 +1,64 @@
+// ES6 => ES2015
+
+// class keyword
+// class are fake
+
+// class CreateUser {
+//   constructor(firstName, lastName, email, age, address) {
+//     this.firstName = firstName;
+//     this.lastName = lastName;
+//     this.email = email;
+//     this.age = age;
+//     this.address = address;
+//   };
+//   about() {
+//     return `${this.firstName} is ${this.age} years`;
+//   };
+//   is18() {
+//     return this.age >= 18;
+//   };
+//   sing() {
+//     return 'la la la';
+//   };
+// };
+
+
+// const user1 = new CreateUser('harshit', 'vashistha', 'harshit@yopmail.com', 45, 'LA, SV, USA');
+
+// // console.log(user1);
+// console.log(Object.getPrototypeOf(user1));
+
+
+// getters and setters
+
+class Person {
+  constructor(firstName, lastName, age) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+  };
+  // getter method
+  get fullName() {// method made behave like a property
+    return `${this.firstName} ${this.lastName}`;
+  };
+
+  // setter method
+  set fullName(fullName) {
+    const [firstName, lastName] = fullName.split(' ');
+    this.firstName = firstName;
+    this.lastName = lastName;
+  };
+};
+
+const person1 = new Person('harshit', 'sharma', 23);
+
+// console.log('person1', person1);
+
+console.log('person1', person1.fullName);
+person1.fullName = 'Amresh Singh';
+
+// console.log('person1', person1);
+
+console.log('person1', person1.fullName);
+
+
