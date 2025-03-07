0832350d8a28cf88c64b18324fbbcfda1a86791c
dibya.borah <dibyajyotiborah469@gmail.com>
2025-03-07 20:30:02 +0530

    Done returning a chunk array problem

--- chunk.js
+++ chunk.js
@@ -12,11 +12,54 @@
 // Input: arr = [1,2,3,4,5], size = 1
 // Output: [[1],[2],[3],[4],[5]]
 
+// Input: arr = [1,9,6,3,2], size = 3
+// Output: [[1,9,6],[3,2]]
+
 /**
  * @param {Array} arr
  * @param {number} size
  * @return {Array}
  */
 
-var chunk = function(arr, size) {
+// const arr = [1,2,3,4,5], size = 1;
+
+// const arr = [42, 10, 19, 6, 8, 7, 9, 325, 0, 428, 23, 757], size = 3;
+
+const arr = [1,9,6,3,2], size = 3;
+// const arr = [1,2,3,4,5], size = 6;
+// const arr = [], size = 1;
+
+// My solution
+// const chunked = [];
+// let chunk = function (arr, size) {
+//   const length = Math.ceil(arr.length / size);
+//   let innerIndex = 0;
+//   for (let index = 0; index < length; index++) {
+//     let temp = []
+//     for(let index_ = 0; index_ < size; index_++) {
+//       if(arr[innerIndex] === undefined) break;
+//       temp[index_] = arr[innerIndex];
+//       innerIndex++
+//     };
+//     chunked[index] = temp;
+//   };
+//   return chunked;
+// };
+
+// console.log('arr', arr);
+// chunk(arr, size);
+// console.log('chunked', chunked);
+
+
+// AI solution
+function chunk(arr, size) {
+  const result = [];
+  for (let i = 0; i < arr.length; i += size) {
+    // Extract a chunk of size 'size' and push it into the result array
+    console.log(i, i + size);
+    result.push(arr.slice(i, i + size));
+  };
+  return result;
 };
+
+console.log('result', chunk(arr, size));
